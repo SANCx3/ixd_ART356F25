4db80dc3ad25deb79252f8eece0674fee5274d01
SANCx3 <156867743+SANCx3@users.noreply.github.com>
2025-10-02 15:19:23 -0700

    Add files via upload

--- MusicShuffler/sketch.js
+++ MusicShuffler/sketch.js
@@ -5,6 +5,24 @@ let image_filelist = [
   "album/album4.jpg",
   "album/album5.jpg",
   "album/album6.jpg",
+  "album/album7.png",
+  "album/album8.png",
+  "album/album9.png",
+  "album/album10.png",
+  "album/album11.png",
+  "album/album12.png",
+  "album/album13.png",
+  "album/album14.png",
+  "album/album15.png",
+  "album/album16.png",
+  "album/album17.png",
+  "album/album18.png",
+  "album/album19.png",
+  "album/album20.png",
+  "album/album21.png",
+  "album/album22.png",
+  "album/album23.png",
+  "album/albumpac.png",
 ];
 let imagelist = [];
 let current_image = 1;
@@ -15,6 +33,24 @@ let audio_filelist = [
   "audio/Ghosttbusters.mp3",
   "audio/Ghastbasters .mp3",
   "audio/aqua.mp3",
+  "audio/PacMansPark.mp3",
+  "audio/Biollante.mp3",
+  "audio/MENU.mp3",
+  "audio/Esperanto.mp3",
+  "audio/Sista.mp3",
+  "audio/Ghostbusters Afterlife.mp3",
+  "audio/Almost Dead.mp3",
+  "audio/I Shoulda Known!!.mp3",
+  "audio/Bamboo.mp3",
+  "audio/ルパン三世80.mp3",
+  "audio/Ghostbusterz.mp3",
+  "audio/Gostbostr.mp3",
+  "audio/Tokyo Speedrun.mp3",
+  "audio/Beat Him.mp3",
+  "audio/cannonball.mp3",
+  "audio/Ska Cha Cha.mp3",
+  "audio/Naked Glow.mp3",
+  "audio/Eatem UP.mp3",
 ];
 let audiolist = [];
 let nowPlaying;
@@ -35,14 +71,18 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
+
+  strokeWeight(4);
   nowPlaying = audiolist[current_image];
-  slider = createSlider(0, 255);
-  slider.position(10, 10);
-  slider.size(100);
 }
 
 function draw() {
   background(174, 252, 242);
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 10);
+    stroke(random(255), random(132), random(245));
+    fill(random(255), random(132), random(245));
+  }
   image(imagelist[current_image], 80, 20, 350, 350);
   //controls
   if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
